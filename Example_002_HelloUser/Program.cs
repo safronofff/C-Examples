353b8d6f50f0516b28ff6f03db1aa072f2b6be50
Yuriy Safronoff <yuriy.safronoff@yandex.ru>
2025-01-10 18:58:30 +0300

    Example_003

--- Example_002_HelloUser/Program.cs
+++ Example_002_HelloUser/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Введите имя: ");
-string username = Console.ReadLine();
-Console.WriteLine("Привет! " + username);
+﻿// Console.WriteLine("Введите имя: ");
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет! " + username);
 
 Console.Write("Enter Username: ");
 string username = Console.ReadLine();
---
+++ Example_003_Summ/Program.cs
@@ -0,0 +1,8 @@
+﻿// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
+
+int numberA = 5;
+int numberB = 9;
+int result = numberA + numberB;
+Console.WriteLine(result);
