82978d02a795d5b4c853eec7174beb04a6847c4b
Yuriy Safronoff <yuriy.safronoff@yandex.ru>
2025-01-10 18:28:45 +0300

    Example_002_HelloUser

---
+++ Example_002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет! " + username);
+
+Console.Write("Enter Username: ");
+string username = Console.ReadLine();
+Console.Write("Hello " + username + "!!!");
