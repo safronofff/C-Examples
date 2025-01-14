68ce5a04e2d7c8430ebf2ebcc4a91938d6f3f2ae
Yuriy Safronoff <yuriy.safronoff@yandex.ru>
2025-01-14 19:45:28 +0300

    Example_011

---
+++ Example_011_Array2/Program.cs
@@ -0,0 +1,17 @@
+﻿//index array  0  1  2   3  4   5  6   7   8  9
+int[] array = {1, 5, 56, 8, 13, 5, 21, 17, 9, 10};
+
+int n = array.Length;
+int find = 5;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // останавливает поиск, когда эл-т найден (например если одинаковых эл-тов 2)
+    }
+    index ++; //index += 1; index = index + 1;
+}
