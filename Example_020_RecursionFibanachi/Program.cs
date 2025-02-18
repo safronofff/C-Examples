772ec4429667bcbc81559a7cfb965cd58849c655
Yuriy Safronoff <yuriy.safronoff@yandex.ru>
2025-02-18 20:16:36 +0300

    Example_020

---
+++ Example_020_RecursionFibanachi/Program.cs
@@ -0,0 +1,15 @@
+﻿// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double f(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return f(n-1) + f(n-2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"f({i}) = {f(i)}");   
+}
+
