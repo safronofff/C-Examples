603826fe2f31db7d0f9dea29d5158efe700efc15
Yuriy Safronoff <yuriy.safronoff@yandex.ru>
2025-02-20 19:28:15 +0300

    Example_023

---
+++ Example_023_Recursion-7_Smma_1-n/Program.cs
@@ -0,0 +1,18 @@
+﻿// Сумма чисел от 1 до n
+
+//Итеративный метод
+int SumFor(int n)
+{
+ int result = 0;
+ for (int i = 1; i <= n; i++) result += i;
+ return result;
+}
+
+//Рекурсивный метод
+int SumRec(int n)
+{
+ if (n == 0) return 0;
+ else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumFor(10)); // 55
+Console.WriteLine(SumRec(10)); // 55
