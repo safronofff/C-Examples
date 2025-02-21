137032526e2c1c21829594cd7918990c58ccbd42
Yuriy Safronoff <yuriy.safronoff@yandex.ru>
2025-02-21 15:53:44 +0300

    Example_025

---
+++ Example_025__Recursion-7_Stepen_Chisla_A/Program.cs
@@ -0,0 +1,28 @@
+﻿// Вычислить степень числа А
+
+//Итеративный метод
+int PowerFor(int a, int n)
+{   int result = 1;
+    for (int i = 1; i <= n; i++) result *= a;
+    return result;
+}
+
+// Рекурсивный метод
+int PowerRec(int a, int n)
+{   // return n == 0 ? 1 : PowerRec(a, n - 1) * a; // Тернарный оператор
+    if (n == 0) return 1;
+    else return PowerRec(a, n - 1) * a;
+}
+
+
+int PowerRecMath(int a, int n)
+{
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return PowerRecMath(a, n - 1) * a;
+}
+
+
+Console.WriteLine(PowerFor(2, 10)); // 1024
+Console.WriteLine(PowerRec(2, 10)); // 1024
+Console.WriteLine(PowerRecMath(2, 10)); // 1024
