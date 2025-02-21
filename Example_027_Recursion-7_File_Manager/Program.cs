c507aa16f52d084e08b4fce3227878f2e9cba339
Yuriy Safronoff <yuriy.safronoff@yandex.ru>
2025-02-21 17:08:45 +0300

    Example_027

---
+++ Example_027_Recursion-7_File_Manager/Program.cs
@@ -0,0 +1,16 @@
+﻿void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+    foreach (var currentCatalog in catalogs.GetDirectories())
+    {
+        Console.WriteLine($"{indent}{currentCatalog.Name}");
+        CatalogInfo(currentCatalog.FullName, indent + " ");
+    }
+    foreach (var item in catalogs.GetFiles())
+    {
+        Console.WriteLine($"{indent}{item.Name}");
+    }
+}
+
+string path = @"C:\Users\Юра\Desktop\обучение_2\C#\Examples\Example_002_HelloUser";
+CatalogInfo(path);
